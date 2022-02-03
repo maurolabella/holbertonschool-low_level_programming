da3b83a326c4b7bfcf0a2bd0aff7c23ec2e3d62e
maurolabella <maurolabella@yahoo.com>
2022-02-03 14:32:35 -0800

    first 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - main
+ * Return: algo
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long longType;
+	long long longlongType;
+	float floatType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	return (0);
+}
