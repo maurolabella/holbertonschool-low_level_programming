66ad368f66548f7fdbeec9e58595169f3f632dcc
maurolabella <maurolabella@yahoo.com>
2022-02-04 15:52:29 -0800

    first 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+ * main - main
+ * Return: algo
+ */
+int main(void)
+
+{
+
+int i = 0;
+int j = 0;
+int k = 0;
+
+for (i; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+
+for (k = j + 1; k < 10; k++)
+{
+putchar(i % 10 + '0');
+putchar(j % 10 + '0');
+putchar(k % 10 + '0');
+
+if (!(i == 7 && j == 8 && k == 9))
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+}
+}
+putchar('\n');
+return (0);
+}
