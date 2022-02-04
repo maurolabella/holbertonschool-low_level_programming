6af4cfc8c027b8c01b6dbb54a7361aaf60c134ce
maurolabella <maurolabella@yahoo.com>
2022-02-04 08:50:48 -0800

    first comm 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+ * main - main
+ * Return: algo
+ */
+int main(void)
+
+{
+
+int ch;
+char sl = '\n';
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar(sl);
+
+return (0);
+
+}
