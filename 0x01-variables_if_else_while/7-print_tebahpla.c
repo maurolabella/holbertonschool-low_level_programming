3dae8023b1893e3689862671f6d3a4a88dd1a034
maurolabella <maurolabella@yahoo.com>
2022-02-04 13:14:29 -0800

    first 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+ * main - main
+ * Return: algo
+ */
+int main(void)
+
+{
+
+int ch;
+
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+
+putchar(ch);
+
+}
+
+putchar('\n');
+
+return (0);
+}
