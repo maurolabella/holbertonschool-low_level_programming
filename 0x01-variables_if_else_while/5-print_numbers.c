3ee98ded591f57961c34df03de5ec27e9cd19c96
maurolabella <maurolabella@yahoo.com>
2022-02-04 09:38:44 -0800

    firsst 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+ * main - main
+ * Return: algo
+ */
+int main(void)
+
+{
+
+int ch;
+char sl = '\n';
+
+for (ch = 0; ch <= 10; ch++)
+{
+putchar(ch);
+}
+
+putchar(sl);
+
+return (0);
+}
