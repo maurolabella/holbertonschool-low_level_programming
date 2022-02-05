5a8dc9c55c7b980883ce0877fc4906962431dd74
maurolabella <maurolabella@yahoo.com>
2022-02-04 19:38:47 -0800

    first commit 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+ * main - main
+ * Return: algo
+ */
+int main(void)
+
+{
+
+int i = 0;
+int j = 0;
+int k = 0;
+int l = 0;
+while (i < 10)
+{
+while (j < 10)
+{
+while (k < 10)
+{
+while (l < 10)
+{
+if (i == k && l > j)
+{
+putchar(i % 10 + '0');
+putchar(j % 10 + '0');
+putchar(' ');
+putchar(k % 10 + '0');
+putchar(l % 10 + '0');
+if (!(i == 9 && j == 8 && k == 9 && l == 9))
+{putchar(',');
+putchar(' ');
+}
+}
+if (k > i)
+{
+putchar(i % 10 + '0');
+putchar(j % 10 + '0');
+putchar(' ');
+putchar(k % 10 + '0');
+putchar(l % 10 + '0');
+if (!(i == 9 && j == 8 && k == 9 && l == 9))
+{putchar(',');
+putchar(' ');
+}
+}
+l++;
+}
+k++;
+l = 0;
+}
+j++;
+k = i;
+}
+i++;
+j = 0;
+}
+putchar('\n');
+return (0);
+}
