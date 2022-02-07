e574a210b4e499b0d473a7afcadbe80cbe1cc054
maurolabella <maurolabella@yahoo.com>
2022-02-07 09:55:49 -0800

    h

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * print_alphabet - print_alphabet
+ * Return: algo
+ */
+void print_alphabet(void)
+{
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+
+/**
  * print_alphabet_x10 - print_alphabet
  * Return: algo
  */
