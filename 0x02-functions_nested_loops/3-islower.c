f6e0834ad25f947a1789e573ed3a4bcae8c32bf1
maurolabella <maurolabella@yahoo.com>
2022-02-07 10:08:15 -0800

    h

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,4 @@
-#include <stdio.h>
-#include <ctype.h>
-#include "_putchar.c"
-
-/**
- * print_alphabet - print_alphabet
- * Return: algo
- *
-*/
-void print_alphabet(void)
-{
-for (char a = 'a'; a <= 'z'; a++)
-{
-_putchar(a);
-}
-}
+#include<ctype.h>
 /**
  * _islower - detect lowercases
  * Return: algo
@@ -29,23 +14,4 @@ else
 {
 return (0);
 }
-}
-
-/**
- * main - main
- * Return: algo
- */
-int main(void)
-{
-int r;
-
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
-
-return (0);
 }
