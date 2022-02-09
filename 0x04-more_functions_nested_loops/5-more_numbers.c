328ca7f243e6c1d96543b9b19b7892de40d3a6a8
maurolabella <maurolabella@yahoo.com>
2022-02-09 07:51:09 -0800

    l

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,21 @@
  */
 void more_numbers(void)
 {
-int i, j;
-for (j = 0; j < 11; j++)
-{
-for (i = 0; i < 15; i++)
-{
-int d;
-d = i / 10;
+	int i, j;
 
-if (!(d == 0))
-_putchar(d + '0');
+	for (j = 0; j < 11; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			int d;
 
-_putchar((i % 10) + '0');
-}
-_putchar('\n');
-}
+			d = i / 10;
+
+			if (!(d == 0))
+				_putchar(d + '0');
+
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
