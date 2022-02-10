bdb0e6009b2a377f1f778b022ab1cf8ae4e7b255
maurolabella <maurolabella@yahoo.com>
2022-02-09 17:14:08 -0800

    a

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triagle
+ * @size: tamaño
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (i = 1; i <= size; i++)
+	{
+		for (j = i; j < size; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+			_putchar('\n');
+	}
+	}
+
+}
