851a639905bf198d26f7b64db125a9c484270bf2
maurolabella <maurolabella@yahoo.com>
2022-02-11 12:17:37 -0800

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - reverse string
+ * @s: pointer
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+int i;
+int len;
+int temp;
+
+len = strlen(s);
+
+        for (i = 0; i < len / 2; i++)
+        {
+                temp = s[i];
+                s[i] = s[len - i - 1];
+                s[len - i - 1] = temp;
+	}
+}
