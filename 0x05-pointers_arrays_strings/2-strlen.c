4819532dbcb0b7f112f3a1b2b2122f82479e9f38
maurolabella <maurolabella@yahoo.com>
2022-02-11 08:46:34 -0800

    a

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s : receives a char
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
