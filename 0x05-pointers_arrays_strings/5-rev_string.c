91082351f4274d5d94af3b79c6ef8e55383cd5f1
maurolabella <maurolabella@yahoo.com>
2022-02-11 12:26:42 -0800

    h

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,10 +14,10 @@ int temp;
 
 len = strlen(s);
 
-        for (i = 0; i < len / 2; i++)
-        {
-                temp = s[i];
-                s[i] = s[len - i - 1];
-                s[len - i - 1] = temp;
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
 }
