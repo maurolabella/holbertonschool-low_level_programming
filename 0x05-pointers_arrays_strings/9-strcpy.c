85d7c6eabff7c7a25ca8a9fe2a6bd6d76fb29a6b
maurolabella <maurolabella@yahoo.com>
2022-02-11 17:51:04 -0800

    got it

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - function to print an array
+ * Return: char
+ * @dest: dest pointer
+ * @src: src pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
