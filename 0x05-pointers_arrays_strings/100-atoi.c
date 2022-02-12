08351dafb59d2d85a8acbf2cd4574dbcca13ce01
maurolabella <maurolabella@yahoo.com>
2022-02-11 18:43:40 -0800

    a

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include<stdlib.h>
+#include "main.h"
+/**
+ * _atoi - filter and convert a string in a integer
+ * Return: an integer
+ * @s : a string is received
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
+
