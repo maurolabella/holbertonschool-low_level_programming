eab4a11b028803d553638ac7688f805c7ff80f1c
maurolabella <maurolabella@yahoo.com>
2022-02-11 17:24:58 -0800

    a

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < (n - 1); i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d, ", a[i]);
+
+		if (j < (n - 1))
+			printf(",");
 	}
-	printf("%d\n", a[n - 1]);
+	printf("\n");
 }
 
