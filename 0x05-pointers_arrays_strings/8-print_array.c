7cfec56bd67289834368099d8c17ac7682e6d6ef
maurolabella <maurolabella@yahoo.com>
2022-02-11 17:16:45 -0800

    a

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,18 +10,11 @@
 void print_array(int *a, int n)
 {
 	int i;
-	int len;
 
-	len = sizeof(a);
-
-	if (len >= n)
+	for (i = 0; i < (n - 1); i++)
 	{
-		for (i = 0; i < (n - 1); i++)
-		{
-			printf("%d, ", a[i]);
-		}
-		printf("%d\n", a[n - 1]);
-
+		printf("%d, ", a[i]);
 	}
+	printf("%d\n", a[n - 1]);
 }
 
