1f0a394975643951b0cfd17d4525f0f06795fe76
maurolabella <maurolabella@yahoo.com>
2022-02-21 10:45:41 -0800

    h

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - find location of first total match of a set substring
+ * @haystack : the mayor string to look for in
+ * @needle : string defines to string with which compare
+ * Return: a pointer towards the beginning of the matched coincidence
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *tmp = needle;
+	int needle_length = 0;
+	int count = 0;
+
+	while (*tmp != '\0')
+	{
+		needle_length++;
+		tmp++;
+	}
+
+	tmp = needle;
+
+	while (*haystack != '\0')
+	{
+		if (*haystack == *tmp)
+		{
+			count++;
+			tmp++;
+		}
+		else
+		{
+			count = 0;
+			tmp = needle;
+		}
+		if (count == needle_length)
+		{
+			return (haystack - needle_length);
+		}
+		haystack++;
+	}
+	return (0);
+}
