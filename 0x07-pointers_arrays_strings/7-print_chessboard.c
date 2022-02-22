88769178d644317fed4f4bbb9e6c4eea3ebc5b6d
maurolabella <maurolabella@yahoo.com>
2022-02-21 16:30:34 -0800

    a

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * print_chessboard - check the code
+ * @a receives an two-dimension array
+ * Return: nothing.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int dimension, i, j;
+
+	dimension = sizeof(a[0]);
+
+	for (i = 0; i < dimension; i++)
+		for (j = 0; j < dimension; j++)
+		{
+			if (j == (dimension - 1))
+			{
+				_putchar(a[i][j]);
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(a[i][j]);
+			}
+		}
+}
