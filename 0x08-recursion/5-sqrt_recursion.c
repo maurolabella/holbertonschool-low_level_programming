6e9323e650a54da1fa7708eba48661157ae3887a
maurolabella <maurolabella@yahoo.com>
2022-02-23 13:46:59 -0800

    a

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * root1 - find a root from an initial guess
+ * @n : integer to be root squared
+ * @i : initial guess
+ * Return: an integer.
+ */
+int root1(int n, int i)
+{
+	if (n - i * i == 0)
+	{
+		return (i);
+	}
+	else
+	{
+		if (n - i * i > 0)
+		{
+			root1(n, i + 1);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	return (i);
+}
+
+
+/**
+ * _sqrt_recursion - cites a recursion function to find the root square
+ * @n : receives the number to be root squared
+ * Return: the result as an integer.
+ */
+int _sqrt_recursion(int n)
+{
+	return (root1(n, 1));
+}
