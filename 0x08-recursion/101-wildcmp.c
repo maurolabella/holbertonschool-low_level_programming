da8575a9d86185affd23e18da2730b03c2174e1c
maurolabella <maurolabella@yahoo.com>
2022-02-24 17:22:36 -0800

    a

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,58 +10,40 @@
  * @type : char
  * Return: an integer.
  */
+
 int true_compare(char *s1, char *s2, char type, char *mem)
 {
 	if (*s2 == '\0' && *s1 == '\0')
-	{
 		return (1);
-	}
 	else if (*s1 == '\0' && *s2 != '*')
-	{
 		return (0);
-	}
 	else if (*s1 != '\0' && *s1 == '\0')
-	{
 		return (0);
+
+	if (*s1 == *s2)
+	{
+		if (type == 'c')
+			return (true_compare((++s1), (++s2), 'c', mem));
+		else
+			return (true_compare((++s1), (++s2), 'o', mem));
+	}
+	else if (*s2 == '*')
+	{
+		if (*s1 == '\0' && strlen(s2) == 1)
+			return (1);
+		s2++;
+		mem = s2;
+		return (true_compare(s1, s2, 'c', mem));
 	}
 	else
 	{
-		if (*s1 == *s2)
+		if (type == 'c')
 		{
-			if (type == 'c')
-			{
-				return (true_compare((++s1), (++s2), 'c', mem));
-			}
-			else
-			{
-				return (true_compare((++s1), (++s2), 'o', mem));
-			}
-		}
-		else if (*s2 == '*')
-		{
-			if (*s1 == '\0' && strlen(s2) == 1)
-			{
-				return (1);
-			}
-			else
-			{
-				s2++;
-				mem = s2;
-				return (true_compare(s1, s2, 'c', mem));
-			}
+			(s2 != mem) ? (s2 = mem) : (s1++);
+			return (true_compare(s1, s2, 'c', mem));
 		}
 		else
-		{
-			if (type == 'c')
-			{
-				(s2 != mem) ? (s2 = mem) : (s1++);
-				return (true_compare(s1, s2, 'c', mem));
-			}
-			else
-			{
-				return (0);
-			}
-		}
+			return (0);
 	}
 }
 
