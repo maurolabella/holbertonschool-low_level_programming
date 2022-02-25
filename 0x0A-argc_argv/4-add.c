a4b03dcc4b5058096eda148413ff0283f194ea1e
maurolabella <maurolabella@yahoo.com>
2022-02-25 08:36:58 -0800

    a

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.h"
+
+/**
+ * main - do
+ * @argc : integer
+ * @argv : array of pointer to arguments
+ * Return: an integer.
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+	int i;
+
+	result = 0;
+
+	i = 1;
+
+	while (argv[i])
+	{
+		if (*argv[i] != '0' && atoi(argv[i]) == 0)
+		{
+			printf("Error");
+			return (1);
+		}
+		i++;
+	}
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			result = result + atoi(argv[i]);
+		}
+		printf("%d\n", result);
+	}
+	return (0);
+}
