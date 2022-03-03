51011aba151189628bd8bfc145772e5232c31ded
maurolabella <maurolabella@yahoo.com>
2022-03-03 07:46:02 -0800

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,25 +13,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p = NULL;
 	int i, t = 0;
-	unsigned int j = 0;
+	unsigned int j, k = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
+	(strlen(s2) > n) ? (k = n) : (k = strlen(s2));
 
-	p = malloc(strlen(s1) + strlen(s2) + 1);
+	p = malloc(strlen(s1) + k + 1);
 	if (p != NULL)
 	{
 		for (i = 0; s1[i]; i++)
 		{
 			p[t++] = s1[i];
 		}
-
 		for (j = 0; (s2[j] && (j < n)); j++)
 		{
 			p[t++] = s2[j];
