7434530b4c1fb14d3fecea2a7a9614c1fa6432cb
maurolabella <maurolabella@yahoo.com>
2022-03-08 20:54:53 +0000

    a

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
+
+/**
+ * new_dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *Return: void
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *new_name = strdup(name);
+	char *new_owner = strdup(owner);
+	dog_t *new_dog;
+
+	if (new_name == NULL)
+		return (NULL);
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		return (NULL);
+	}
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
+}
