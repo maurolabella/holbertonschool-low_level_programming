b01e0050deb95df6f09953ca488a86e9e1d8de93
maurolabella <maurolabella@yahoo.com>
2022-03-09 00:13:12 +0000

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,7 +4,6 @@
 
 /**
  * new_dog - Short description
- * @d: pointer
  * @name: First member
  * @age: Second member
  * @owner: Third member
@@ -12,23 +11,39 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_perro;
+	int i;
+	dog_t *perro_new;
 
-	if (name == NULL)
+	perro_new = malloc(sizeof(dog_t));
+	if (perro_new)
 	{
-		return (NULL);
-	}
+		perro_new->name = malloc(strlen(name) + 1);
+		if (perro_new->name)
+		{
+			for (i = 0; name[i]; i++)
+				perro_new->name[i] = name[i];
+			perro_new->name[i] = '\0';
+		}
+		else
+		{
+			free(perro_new);
+			return (NULL);
+		}
+		perro_new->age = age;
 
-	if (owner == NULL)
-	{
-		return (NULL);
+		perro_new->owner = malloc(strlen(owner) + 1);
+		if (perro_new->owner)
+		{
+			for (i = 0; owner[i]; i++)
+				perro_new->owner[i] = owner[i];
+			perro_new->owner[i] = '\0';
+		}
+		else
+		{
+			free(perro_new->name);
+			free(perro_new);
+			return (NULL);
+		}
 	}
-
-	new_perro = malloc(sizeof(dog_t));
-	if (new_perro == NULL)
-		return (NULL);
-	new_perro->name = name;
-	new_perro->age = age;
-	new_perro->owner = owner;
-	return (new_perro);
+	return (perro_new);
 }
