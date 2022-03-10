5c59ff4e8b1a6ba164c45b7990041a987f60835c
maurolabella <maurolabella@yahoo.com>
2022-03-10 13:27:48 +0000

    a

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,25 +7,27 @@
 /**
  * get_op_func - function operator
  * @s: operator passed as arguments
- * @int: operation's first operator
- * @int: operations's second operator
  * Return: a pointer to a function
  */
 int (*get_op_func(char *s))(int, int)
 {
 	int i;
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}};
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
 
-	for (i = 0; ops[i].op; i++)
+	i = 0;
+
+	while (ops[i].op)
 	{
 		if (ops[i].op[0] == s[0])
 			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
