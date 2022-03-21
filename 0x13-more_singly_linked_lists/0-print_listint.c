d132b4d435bf0992582873d00705a9f2c65baa36
maurolabella <maurolabella@yahoo.com>
2022-03-21 17:38:40 +0000

    a

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *  print_listint - print all the elements of a list
+ * @author mauro.labella
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	if (h == NULL)
+		return (n);
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
