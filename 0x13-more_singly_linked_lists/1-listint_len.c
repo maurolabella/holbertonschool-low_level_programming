e1ddcdb2501a4a045ead821f95de192dadefbd1a
maurolabella <maurolabella@yahoo.com>
2022-03-21 17:57:24 +0000

    a

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked list
+ *
+ * @h: head of the list (received)
+ * Return: size_t  elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
