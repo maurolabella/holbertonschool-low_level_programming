303133958fa4aa9a7e53a9e26ec84d6eea046e7b
maurolabella <maurolabella@yahoo.com>
2022-03-22 22:12:38 +0000

    a

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		else
 			break;
 	}
-	if (tmp == NULL || ((i + 1) != idx))
+	if (tmp == NULL)
 		return (NULL);
 	new->next = tmp->next;
 	tmp->next = new;
