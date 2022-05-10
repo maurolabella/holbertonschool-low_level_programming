8e75d0bf02eb78a37111a872838d42a76a17d5f8
maurolabella <maurolabella@yahoo.com>
2022-05-09 22:28:24 -0300

    A

---
+++ 0x18-dynamic_libraries/100-main.h
@@ -0,0 +1,11 @@
+#ifndef _CIEN_OPERATIONS_
+#define _CIEN_OPERATIONS_
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif
---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "100-main.h"
+
+/**
+ * add - calculates mod
+ *
+ * @a: first parameter
+ * @b: second parameter
+ * Return: result as int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - calculates mod
+ *
+ * @a: first parameter
+ * @b: second parameter
+ * Return: result as int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - calculates mod
+ *
+ * @a: first parameter
+ * @b: second parameter
+ * Return: result as int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - calculates div
+ *
+ * @a: first parameter
+ * @b: second parameter
+ * Return: result as int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - calculates mod
+ *
+ * @a: first parameter
+ * @b: second parameter
+ * Return: result as int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
