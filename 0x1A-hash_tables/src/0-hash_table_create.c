f29c334ada437b3ec17e33aba02bb853c6b7bd6d
maurolabella <maurolabella@yahoo.com>
2022-05-26 23:22:10 -0300

    a

--- 0x1A-hash_tables/src/0-hash_table_create.c
+++ 0x1A-hash_tables/src/0-hash_table_create.c
@@ -24,7 +24,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		new_table->array = malloc(sizeof(hash_node_t *) * size);
 
 		if (!new_table->array)
-			return (NULL);
+			free(new_table);
+		return (NULL);
 
 		/* initialize node pointers to NULL */
 		for (i = 0; i < size; i++)
