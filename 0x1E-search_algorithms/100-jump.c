cadfd80e81da95caba2b23f55c6cffaf4a63ec81
maurolabella <maurolabella@yahoo.com>
2022-08-26 21:52:15 -0300

    a

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -27,44 +27,32 @@ unsigned int min(unsigned int a, unsigned int b)
  */
 int jump_search(int *array, size_t size, int value)
 {
-
 	unsigned int step = sqrt(size);
-	/*
-	 * Finding the block where element is
-	 * present (if it is present)
-	 */
 	unsigned int prev = 0;
 
-  printf("Value checked array[%i] = [%i]\n", prev, array[prev]);
+	printf("Value checked array[%i] = [%i]\n", prev, array[prev]);
 
 	while (array[min(step, size) - 1] < value)
 	{
-		printf("Value checked array[%i] = [%i]\n", min(step, size) - 1, array[min(step, size) - 1]);
+		printf("Value checked array[%i] = [%i]\n", (min(step, size) - 1),
+					 array[min(step, size) - 1]);
 		prev = step;
 		step += sqrt(size);
 		if (prev >= size)
 			return (-1);
 	}
 
-	/*
-	 * Doing a linear search for x in block
-	 * beginning with prev.
-	 */
-	printf("Value found between indexes [%i] and [%i]\n", prev, min(step, size) - 1);
+	printf("Value found between indexes [%i] and [%i]\n", prev,
+				 (min(step, size) - 1));
 
 	while (array[prev] < value)
 	{
 		printf("Value checked array[%i] = [%i]\n", prev, array[prev]);
 		prev++;
 
-		/*
-		 *If we reached next block or end of
-		 * array, element is not present.
-		 */
 		if (prev == min(step, size))
 			return (-1);
 	}
-	/* If element is found */
 	if (array[prev] == value)
 		return (prev);
 
